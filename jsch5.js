41bf56be1f2f8d063e2a6c510d3053a44465da79
Ryvqi <rifqikhabibi03@gmail.com>
2024-04-04 21:58:24 +0700

    yowes

---
+++ jsch5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("video boss fight");
+});
+
+audio.addEventListener("click", function audio() {
+    alert("Ini audio");
+});
+
+gambar.addEventListener("click", function gambar() {
+    alert("ini gambar armored core 6");
+});
